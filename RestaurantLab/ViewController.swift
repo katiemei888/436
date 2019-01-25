40b969644d673602860c664030f8699d1e53fc3b
Katherine Mei <ktmei@csc302x32.csc.calpoly.edu>
2019-01-25 10:17:17 -0800

    Add files

--- RestaurantLab/ViewController.swift
+++ RestaurantLab/ViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var label: UILabel!
+    
+    var cities = [String]()
+    var restaurants: [String: [String]] = [:]
+    var selectedCity = ""
+    var selectedRestaurant = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getRestaurantData()
     }
-
-
+    
+    //MARK: Picker Data source
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (component == 0) {
+            return cities.count
+        } else {
+            return restaurants[selectedCity]!.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (component == 0) {
+            return cities[row]
+        } else {
+            return restaurants[selectedCity]![row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (component == 0) {
+            selectedCity = cities[row]
+            selectedRestaurant = restaurants[selectedCity]![0]
+            pickerView.reloadComponent(1)
+            pickerView.selectRow(0, inComponent: 1, animated: true)
+        } else {
+            selectedRestaurant = restaurants[selectedCity]![row]
+        }
+        updateLabel()
+    }
+    
+    //MARK: Private Methods
+    private func getRestaurantData() {
+        if let url = Bundle.main.url(forResource: "Restaurants", withExtension: "plist") {
+            do {
+                let data = try Data(contentsOf: url)
+                let tempDict = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as! [String: [String]]
+                
+                cities = Array(tempDict.keys).sorted()
+                for city in cities {
+                    restaurants[city] = Array(tempDict[city]!).sorted()
+                }
+                selectedCity = cities[0]
+                selectedRestaurant = restaurants[selectedCity]![0]
+                updateLabel()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    private func updateLabel() {
+        label.text = "\(selectedCity) – \(selectedRestaurant)"
+    }
 }
-
